90fc336cfaa8c2ec3c68798807e61b2fc2b1d573
Ojas Mahajan <mahajanojas1412@gmail.com>
2025-10-04 01:07:04 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1VxR0VxTB6G8tAg9cLecqEfoasv2VVAGi
 """
 
-!pip install yfinance plotly streamlit prophet pandas numpy pytz
 
 import yfinance as yf
 import pandas as pd
